9781c8faa0fbb454250e569fb6ad1a01631d3440
Abraham Milgram <58184483+abraham-milgram@users.noreply.github.com>
2021-10-13 09:39:12 -0400

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,14 @@
+name0 = input('''
+Please enter a two names seperated by an enter:
+
+''')
+name1 = input()
+adjective = input('''
+Please enter an adjective:
+
+''')
+noun = input('''
+Please enter a noun:
+
+''')
+print(f"{name0} hates {name1}. It's because {name1} is a {adjective} little {noun}. Maybe if {name1} wasn't so {adjective}, {name0} and {name1} could be freinds.")
